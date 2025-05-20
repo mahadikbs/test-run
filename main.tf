f37037249572c5dce5bdf06bd75effe36f0c0351
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-05-20 08:54:31 +0530

    add tf stuff

---
+++ main.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-security-group"
+  description = "Allow SSH and Jenkins access"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH access
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow Jenkins access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "jenkins_server" {
+  ami           = "ami-0abcdef1234567890" # Replace with latest Ubuntu AMI
+  instance_type = "t2.medium"
+  key_name      = "your-key-pair"
+  security_groups = [aws_security_group.jenkins_sg.name]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update -y
+    sudo apt install -y openjdk-11-jdk
+    wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+    sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+    sudo apt update -y
+    sudo apt install -y jenkins
+    sudo systemctl enable jenkins
+    sudo systemctl start jenkins
+  EOF
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
